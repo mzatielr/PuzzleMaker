ceb315191276f3e54555702f7f6bd38decf1095a
Dannynis <dannynis1996@gmail.com>
2017-10-18 18:11:13 +0300

    working new PCA

--- Piece_Detection/KMeansConsoleApplication/KMeansConsoleApplication/Source.cpp
+++ Piece_Detection/KMeansConsoleApplication/KMeansConsoleApplication/Source.cpp
@@ -1,18 +1,18 @@
+
 #include "opencv2/highgui.hpp"
 #include "opencv2/core.hpp"
 #include "opencv2/imgproc.hpp"
-
+#include <exception>
 #include <iostream>
 #include <thr/xthrcommon.h>
-#include <ctime>
-/* time_t, struct tm, difftime, time, mktime */
 
 using namespace cv;
 using namespace std;
 
 // Edge Detection area
 /// Global variables
-
+int neutralColor ;
+Mat src1; //will allways contain the original image ONLY
 Mat src, src_gray;
 Mat dst, detected_edges;
 
@@ -22,10 +22,7 @@ int const max_lowThreshold = 100;
 int ratio = 3;
 int kernel_size = 3;
 char* window_name = "Edge Map";
-bool isUIMode = false;
-
-const double alpha = 1.0 / 255.0; // Don't change const value
-const int K = 10;
+bool isUIMode = true;
 
 string path1;
 string path2;
@@ -39,41 +36,99 @@ void ShowImage(const Mat& src, const string& name)
 		imshow(name, src);
 	}
 }
+//function to blacken the background from the ORIGINAL image
+//todo: not working !! 
+void blackenOrigImage(Mat& blackenClustered)
+{
+	int i, j;
+	int a, b;
+	setbuf(stdout, NULL);
+	double alpha = 1.0 / 255.0;
+	Mat origBlacken = src1.clone();
+	origBlacken.convertTo(origBlacken, CV_32FC3, alpha);
+	
+	for (int i = 0; i<blackenClustered.cols*blackenClustered.rows; i++)
+	{
+		a = i / blackenClustered.cols;
+		b = i % blackenClustered.cols;
+	
+		if (blackenClustered.at<float>(Point(a,b )) == static_cast<float>(neutralColor))// access violation thrown at this line, why ?
+		{
+			origBlacken.at<Vec3f>(a,b).val[0] = 0;
+			//cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
+			origBlacken.at<Vec3f>(a,b).val[1] = 0;
+			origBlacken.at<Vec3f>(a,b).val[2] = 0;
+		}
+	
+	
+	
+	}
 
+	/*
+		for ( i = 0; i < blackenClustered.rows; ++i)
+		{
+			for ( j = 0; j < blackenClustered.cols; ++j)
+			{
+				cout << j << "\\" << blackenClustered.cols << " \n";
+			//	cout << j << "\\" << origBlacken.cols << " \n";
+
+
+				if (blackenClustered.at<float>(Point(i, j)) == static_cast<float>( 0) )// access violation thrown at this line, why ?
+				{
+					origBlacken.at<Vec3f>(i, j).val[0] = 0;
+					//cout << "aaaaaaaaaaaaaaaaaaaaaaaaaaa\n";
+					origBlacken.at<Vec3f>(i, j).val[1] = 0;
+					origBlacken.at<Vec3f>(i, j).val[2] = 0;
+				}
+			}
+
+		}
+	*/
+	
+	ShowImage(origBlacken, "finalOutPut\n");
+}
 
 void RemoveClusters(Mat& src)
 {
-	Rect* rect = new Rect();
+	cout << "Starting cluster removal...\n";
+	Rect* rect= new Rect();
 	double alpha = 1.0 / 255.0;
 	Mat mask = Mat::zeros(src.rows + 2, src.cols + 2, CV_8UC1);
 
+
+	neutralColor = 1;
 	// East
 	for (int i = 0; i < src.rows; ++i)
 	{
-		floodFill(src, mask, Point(0, i), 0, rect, cvScalarAll(0), cvScalarAll(0), 4 | (255 << 8));
+		floodFill(src, mask, Point(0, i), neutralColor, rect, cvScalarAll(0), cvScalarAll(0), 8 | (255 << 8));
 	}
 
 	// West
 	for (int i = 0; i < src.rows; ++i)
 	{
-		floodFill(src, mask, Point(src.cols - 1, i), 0, rect, cvScalarAll(0), cvScalarAll(0), 4 | (255 << 8));
+		floodFill(src, mask, Point(src.cols - 1, i), neutralColor, rect, cvScalarAll(0), cvScalarAll(0), 8 | (255 << 8));
 	}
 
 	// North
 	for (int i = 0; i < src.cols; ++i)
 	{
-		floodFill(src, mask, Point(i, 0), 0, rect, cvScalarAll(0), cvScalarAll(0), 4 | (255 << 8));
+		floodFill(src, mask, Point(i, 0), neutralColor, rect, cvScalarAll(0), cvScalarAll(0), 8 | (255 << 8));
 	}
 
 	// South
 	for (int i = 0; i < src.cols; ++i)
 	{
-		floodFill(src, mask, Point(i, src.rows - 1), 0, rect, cvScalarAll(0), cvScalarAll(0), 4 | (255 << 8));
+		floodFill(src, mask, Point(i, src.rows - 1), neutralColor, rect, cvScalarAll(0), cvScalarAll(0), 8 | (255 << 8));
 	}
-	imwrite("Mask.bmp", mask);
+	cout << "Finished cluster removal\n";
 
+	imwrite(path3, mask);
 	ShowImage(src, "afterClusterRemoval");
 	ShowImage(mask, "Mask");
+
+	blackenOrigImage(src);
+
+
 
 	return;
 }
@@ -88,7 +143,7 @@ void CannyThreshold(int, void*)
 	blur(src_gray, detected_edges, Size(3, 3));
 
 	/// Canny detector
-	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
+	Canny(detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size);
 
 	/// Using Canny's output as a mask, we display our result
 	dst = Scalar::all(0);
@@ -130,86 +185,25 @@ void EdgeDetector(string path)
 
 int GetRectEdge()
 {
-	return 5;
+	return 7;
 }
 
-
-//if we cant take the serounding of the pixle we will return 0, if we can will return 1
-int GenerateKernel(const Mat& src, int i, int j, Mat& kernel)
+void GenerateKernel(const Mat& src, int i, int j, Mat& kernel)
 {
-
-
 	const int rectEdge = GetRectEdge();
 
 	int shift = (rectEdge - 1) / 2;
-
-	if (i - shift < 0)
-	{
-	//	cout << i - shift << "< 0 \n";
-		return 0;
-	}
-	if (j - shift < 0)
-	{
-	//	cout << j - shift << "< 0 \n";
-		return 0;
-	}
-	if (i + shift >= src.rows)
-	{
-	//	cout << i + shift << ">=" << src.rows<<"\n";
-		return 0;
-	}
-	if (j + shift >= src.cols)
-	{
-	//	cout << j + shift << ">=" << src.cols<<"\n";
-
-		return 0;
-	}
-
 	int cnt = 0;
-
-	int a1 =  i - shift;
-	int a2 =  i + shift;
-	int b1 =  j - shift;
-	int b2 =  j + shift;
-
-	/*
-	int a1 = max(0, i - shift);
-	int a2 = min(src.rows, a1 + rectEdge);
-	
-	if (a1 + rectEdge >= src.rows)
+	for (int row = max(0, i - shift); row <= min(src.rows - 1, i + shift); ++row)
 	{
-		
-		int t = (src.rows - (a1 + rectEdge));
-		a1 -= t;
-	}
-
-	int b1 = max(0, j - shift);
-	int b2 = min(src.cols, b1 + rectEdge);
-	
-	if (b1 + rectEdge >= src.cols)
-	{
-		cout << a1 << ">=" << src.rows << '\n';
-
-		return 0;
-		//int t = (src.cols - (b1 + rectEdge));
-		//b1 -= t;
-	}
-	//cout << "a2: " << a2 << " a1: " << a1 << " b2: " << b2 << " b1: " << b1 << endl;
-
-	*/
-
- 	//CV_Assert(a2 - a1 == rectEdge && b2 - b1 == rectEdge);
-	for (int row = a1; row < a2; ++row)
-	{
-		for (int col = b1; col < b2; ++col)
+		for (int col = max(0, j - shift); col <= min(src.cols - 1, j + shift); ++col)
 		{
-			kernel.at<float>(cnt, 0) = static_cast<float>(src.at<Vec3f>(row, col).val[0]);
-			kernel.at<float>(cnt, 1) = static_cast<float>(src.at<Vec3f>(row, col).val[1]);
-			kernel.at<float>(cnt, 2) = static_cast<float>(src.at<Vec3f>(row, col).val[2]);
+			kernel.at<double>(cnt, 0) = static_cast<double>(src.at<Vec3d>(row, col).val[0]);
+			kernel.at<double>(cnt, 1) = src.at<Vec3d>(row, col).val[1];
+			kernel.at<double>(cnt, 2) = src.at<Vec3d>(row, col).val[2];
 			cnt++;
 		}
 	}
-	return 1;
 }
 
 void PrintMatShort(const Mat& mat)
@@ -235,7 +229,7 @@ void PrintMatDouble(const Mat& mat)
 		}
 		cout << endl;
 	}
-	cout << endl;
+	cout  << endl;
 }
 
 void PrintMatC1(const Mat& mat)
@@ -258,113 +252,7 @@ int GetKernelSize(const int rect_edge, int i, int j, int rows, int cols)
 	int shift = (rect_edge - 1) / 2;
 	int a = min(rows - 1, i + shift) - max(0, i - shift) + 1;
 	int b = min(cols - 1, j + shift) - max(0, j - shift) + 1;
-	return a * b;
-}
-
-void FillDataInPcaInput(Mat& targetMat, int global_cnt, const Mat& kernel)
-{
-	int colCnt = 0;
-	// Todo: Implementation can be improved using mat::reshape
-	for (int i = 0; i < kernel.rows; ++i)
-	{
-		for (int j = 0; j < kernel.cols; ++j)
-		{
-			targetMat.at<float>(global_cnt, colCnt) = kernel.at<float>(i, j);
-			colCnt++;
-		}
-	}
-}
-
-// Input: 
-//		rectEdge - rect size for pca evaluation 
-//		srcConverterd - Type: CV_32FC3, src image to calculate pca new image
-// Output:
-//		imageAfterPca - Type: CV_32FC3, initialized to zeros Mat with equal size as src image
-void ExecutePCA(const int rectEdge, const Mat& srcConverterd, Mat& imageAfterPca)
-{
-	double pcaCoreTimeCounter = 0;
-	time_t temp1, temp2;
-	time_t time1, time2;
-	PCA pca_analysis;
-	int numOfDiscardedPixels = 0;
-	time(&time1);
-	Mat pcaInput = Mat::zeros(srcConverterd.rows * srcConverterd.cols, rectEdge * rectEdge * 3, CV_32FC1);
-	int kernelGenResult;
-	auto emptyArray = noArray();
-	int globalCnt = 0;
-	for (int i = 0; i < srcConverterd.rows; ++i)
-	{
-		for (int j = 0; j < srcConverterd.cols; ++j)
-		{
-			//int kernelSize = GetKernelSize(rectEdge, i, j, srcConverterd.rows, srcConverterd.cols);
-			Mat kernel = Mat::zeros(pcaInput.cols, 3, CV_32FC1);
-			kernelGenResult = GenerateKernel(srcConverterd, i, j, kernel);
-			if (kernelGenResult == 1)
-			{
-				FillDataInPcaInput(pcaInput, globalCnt, kernel);
-			}
-			else{ 
-				numOfDiscardedPixels++;
-		//		cout << i << " "<< j<<'\n'; 
-			
-			}
-			//PrintMatC1(kernel);
-		}
-	}
-	cout <<"discarded "<< numOfDiscardedPixels << " out of " << srcConverterd.cols * srcConverterd.rows<<"\n";
-	time(&time2);
-
-	//pca
-	//Perform PCA analysis
-	time(&temp1);
-	pca_analysis(pcaInput, emptyArray, CV_PCA_DATA_AS_ROW, 3); // reduction to 3 dimensions
-	time(&temp2);
-	pcaCoreTimeCounter += difftime(temp2, temp1);
-	//Mat meanPoint = pca_analysis.mean;
-	Mat projection_result;
-
-	pca_analysis.project(pcaInput, projection_result);
-	CV_Assert(3 == projection_result.cols && projection_result.rows == pcaInput.rows);
-
-
-	for (int i = 0; i < projection_result.rows; ++i)
-	{
-		imageAfterPca.at<Vec3f>(i / imageAfterPca.cols, i % imageAfterPca.cols).val[0] = projection_result.at<float>(i, 0);
-		imageAfterPca.at<Vec3f>(i / imageAfterPca.cols, i % imageAfterPca.cols).val[1] = projection_result.at<float>(i, 1);
-		imageAfterPca.at<Vec3f>(i / imageAfterPca.cols, i % imageAfterPca.cols).val[2] = projection_result.at<float>(i, 2);
-	}
-	
-	std::cout << "All iteration took " << difftime(time2, time1) << endl;
-	cout << "PCA analysis took " << pcaCoreTimeCounter << endl;
-
-	imageAfterPca.convertTo(imageAfterPca, CV_8UC3, 255.0);
-	cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
-
-	cout << projection_result;
-	cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
-
-	ShowImage(imageAfterPca, "8UImageAfterPCA");
-	imwrite("ImageAfterPCA.bmp", imageAfterPca);
-	imageAfterPca.convertTo(imageAfterPca, CV_32FC3, alpha);
-}
-
-// Input: 
-//		imageAfterPca- Type: CV_32FC3, src image to calculate kmeans input mat
-// Output:
-//		kmeansMat - KMeans imput mat. Initialized to:  Mat::zeros(imageAfterPca.rows*imageAfterPca.cols, 1, CV_32FC3);
-void GenerateKmeansInputMat(const Mat& imageAfterPca, Mat& kmeansMat)
-{
-	int counter = 0;
-	for (int i = 0; i < imageAfterPca.rows; i++)
-	{
-		for (int j = 0; j < imageAfterPca.cols; ++j)
-		{
-			kmeansMat.at<Vec3f>(counter, 0)[0] = imageAfterPca.at<Vec3f>(i, j)[0];
-			kmeansMat.at<Vec3f>(counter, 0)[1] = imageAfterPca.at<Vec3f>(i, j)[1];
-			kmeansMat.at<Vec3f>(counter, 0)[2] = imageAfterPca.at<Vec3f>(i, j)[2];
-			counter++;
-		}
-	}
+	return a*b;
 }
 
 void GenerateNeightMatrix(Mat& src1)
@@ -379,62 +267,102 @@ void GenerateNeightMatrix(Mat& src1)
 
 	//ShowImage(src1, "blurred");
 	//PrintMatShort(src1);
+	Mat origBlaken = src1.clone();
 
-
-	src1.convertTo(src1, CV_32FC3, alpha);
+	Mat srcConverterd = src1.clone();
+	double alpha = 1.0 / 255.0;
+	cout << alpha << endl;
+	srcConverterd.convertTo(srcConverterd, CV_64FC3, alpha);
 	//PrintMatDouble(srcConverterd);
 
 	//srcConverterd.convertTo(srcConverterd, CV_8UC3, 255.0);
 	//PrintMatShort(srcConverterd);
 
-	Mat imageAfterPca = Mat::zeros(src1.rows, src1.cols, CV_32FC3);
-	cout << "Starting pca" << endl;
-	ExecutePCA(rectEdge, src1, imageAfterPca);
-	cout << "End of PCA stage" << endl;
+	Mat p = Mat::zeros(src1.cols*src1.rows,  3, CV_64FC1);
+	Mat imageAfterPca = Mat::zeros(src1.rows, src1.cols, CV_64FC3);
+
+	for (int i = 0; i < srcConverterd.rows; ++i)
+	{
+		for (int j = 0; j < srcConverterd.cols; ++j)
+		{
+			int kernelSize = GetKernelSize(rectEdge, i, j, srcConverterd.rows, srcConverterd.cols);
+			Mat kernel = Mat::zeros(kernelSize, 3, CV_64FC1);
+			GenerateKernel(srcConverterd, i, j, kernel);
+
+			//PrintMatC1(kernel);
+			
+			//pca
+			//Perform PCA analysis
+			PCA pca_analysis(kernel, noArray(), CV_PCA_DATA_AS_ROW);
+			Mat meanPoint = pca_analysis.mean;
+			//cout << "Mean: rowsXcols " << meanPoint.rows << "X" << meanPoint.cols << endl;
+			//cout << meanPoint << endl;
+
+			imageAfterPca.at<Vec3d>(i, j).val[0] = meanPoint.at<double>(0, 0);
+			imageAfterPca.at<Vec3d>(i, j).val[1] = meanPoint.at<double>(0, 1);
+			imageAfterPca.at<Vec3d>(i, j).val[2] = meanPoint.at<double>(0, 2);
+
+			// same mean in p
+		}
+	}
+
 	//ShowImage(imageAfterPca, "DoubleImageAfterPCA");
-	//imageAfterPca.convertTo(imageAfterPca, CV_8UC3, 255.0);
+	imageAfterPca.convertTo(imageAfterPca, CV_8UC3, 255.0);
+	ShowImage(imageAfterPca, "8UImageAfterPCA");
 
-	Mat kmeansMat = Mat::zeros(imageAfterPca.rows * imageAfterPca.cols, 1, CV_32FC3);
+	Mat kmeansInput = imageAfterPca.clone(); // with pca
+	//Mat kmeansInput = src1.clone(); // without pca
 
-	GenerateKmeansInputMat(imageAfterPca, kmeansMat);
+
+	kmeansInput.convertTo(kmeansInput, CV_32FC3, alpha);
+
+	Mat kmeansMat = Mat::zeros(kmeansInput.rows*kmeansInput.cols, 1, CV_32FC3);
+	
+	int counter = 0;
+	for (int i = 0; i<kmeansInput.rows; i++)
+	{
+		for (int j = 0; j < kmeansInput.cols; ++j)
+		{
+			kmeansMat.at<Vec3f>(counter, 0)[0] = kmeansInput.at<Vec3f>(i, j)[0];
+			kmeansMat.at<Vec3f>(counter, 0)[1] = kmeansInput.at<Vec3f>(i, j)[1];
+			kmeansMat.at<Vec3f>(counter, 0)[2] = kmeansInput.at<Vec3f>(i, j)[2];
+			counter++;
+		}
+	}
 
 	cout << "Executing kmeans" << endl;
 	Mat bestLabels, centers;
 
+	int const K = 7;
 	kmeans(kmeansMat, K, bestLabels,
-	       TermCriteria(TermCriteria::Type::COUNT + TermCriteria::Type::EPS, 10, 1.0), 1, KMEANS_PP_CENTERS, centers);
+		TermCriteria(TermCriteria::Type::COUNT + TermCriteria::Type::EPS, 10, 1.0), 1, KMEANS_PP_CENTERS, centers);
 
 	cout << "Finish to execute kmeans" << endl;
-
 	int colors[K];
-	for (int i = 0; i < K; i++)
+	for (int i = 0; i<K; i++)
 	{
 		colors[i] = 255 / (i + 1);
 	}
 
-	Mat clustered = Mat(imageAfterPca.rows, imageAfterPca.cols, CV_32F);
-	for (int i = 0; i < imageAfterPca.cols * imageAfterPca.rows; i++)
+	Mat clustered = Mat(kmeansInput.rows, kmeansInput.cols, CV_32F);
+	for (int i = 0; i<kmeansInput.cols*kmeansInput.rows; i++)
 	{
-		clustered.at<float>(i / imageAfterPca.cols, i % imageAfterPca.cols) = static_cast<float>(colors[bestLabels.at<int>(0, i)]);
+		clustered.at<float>(i / kmeansInput.cols, i%kmeansInput.cols) = static_cast<float>(colors[bestLabels.at<int>(0, i)]);
 	}
 
-	clustered.convertTo(clustered, CV_8U);
+	//clustered.convertTo(clustered, CV_8UC1);
 	ShowImage(clustered, "clustered");
-
-	imwrite("ClusterdImage.bmp", clustered);
 
 	RemoveClusters(clustered);
 
-	imwrite("AfterClusterRemoval.bmp", clustered);
 }
 
 void KMeans(string path)
 {
-	Mat src1 = imread(path);
+	src1 = imread(path);
 	if (src1.empty())
 	{
-		cout << "ATTENTION: no image loaded! Stopping Execution\n";
-		return;
+		cout << "ATTENTION: no image loaded!\n";
 	}
 	else
 	{
@@ -449,12 +377,25 @@ void KMeans(string path)
 }
 
 
-int main(int argc, char** argv)
+int main(int argc, char** argv) 
 {
-	KMeans(argv[1]);
-	//KMeans("C:\\oldDesktop\\סדנה\\KMeansConsoleApplication\\KMeansConsoleApplication\\nonwhite.bmp");
+	/*
+	path1 = argv[2];
+	path2 = argv[3];
+	path3 = argv[4];
+	KMeans(argv[1]);*/
+	path1 = "a.jpg";
+	path2 = "b.jpg";
+	path3 = "c.jpg";
+	KMeans("C:\\oldDesktop\\סדנה\\KMeansConsoleApplication\\KMeansConsoleApplication\\nonwhite.bmp");
+
 	waitKey();
 	destroyAllWindows();
 
-	return 0;
+    return 0;
 }
+
+
+
+
+
